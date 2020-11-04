bd236f64ccb24d01f2f31d8c37d62981e472b903
Dan Thompson <dxt053@student.bham.ac.uk>
2020-11-04 17:42:55 +0000

    Main source file updated to use cipher function according to user inputs in command line;all code commented

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -9,6 +9,7 @@
 
 #include "transformChar.hpp"
 #include "processCmdLine.hpp"
+#include "runCaesarCipher.hpp"
 
 
 
@@ -22,15 +23,18 @@ int main(int argc, char* argv[])
   // Options that might be set by the command-line arguments
   bool helpRequested {false};
   bool versionRequested {false};
+  int encryptRequestedTemp {2}; //distinct to other requested variables as require third option "2" whcih refers to lack of user specification
   std::string inputFile {""};
   std::string outputFile {""};
+  std::string cipherKeyTemp {""};
 
   // Process command line arguments - ignore zeroth element, as we know this to
   // be the program name and don't need to worry about it
-  if(!processCmdLine(cmdLineArgs,helpRequested,versionRequested,inputFile,outputFile)){
+  if(!processCmdLine(cmdLineArgs,helpRequested,versionRequested,encryptRequestedTemp,inputFile,outputFile,cipherKeyTemp)){
     std::cerr << "Exiting..."<<std::endl;
     return 1;
   }
+
 
   // Handle help, if requested
   if (helpRequested) {
@@ -41,6 +45,9 @@ int main(int argc, char* argv[])
       << "Available options:\n\n"
       << "  -h|--help        Print this help message and exit\n\n"
       << "  --version        Print version information\n\n"
+      << "  -e|--encrpyt     Indicate request for encryption\n\n"
+      << "  -d|--decrpyt     Indicate request for decryption\n\n"
+      << "  -key +veInt      Cipher Key to be proccessed from +veInt\n\n"
       << "  -i FILE          Read text to be processed from FILE\n"
       << "                   Stdin will be used if not supplied\n\n"
       << "  -o FILE          Write processed text to FILE\n"
@@ -57,6 +64,26 @@ int main(int argc, char* argv[])
     std::cout << "0.1.0" << std::endl;
     return 0;
   }
+
+  //create actual variables for cipher key and encrpytion request
+  size_t cipherKey{0};
+  bool encryptRequested(false);
+  
+  //check for lack of stated arguments for encrpytion and cipher key by searching for default arguments
+  if (encryptRequestedTemp==2){
+    std::cerr<<"Encryption/Decryption argument not stated"<<"...Exiting"<<std::endl;
+    return 1;
+  }else{
+    encryptRequested=(bool) encryptRequestedTemp; //if encrypt/decrypt has been stated, cast 0/1 integer to false/true
+  }
+  //user should not be able to enter "" manually...
+  if (cipherKeyTemp.empty()){
+    std::cerr<<"Cipher key not provided by user"<<"...Exiting"<<std::endl;
+    return 1;
+  }else{
+    cipherKey=std::stoul(cipherKeyTemp); //if a key has been entered by user, can convert already validated postive integer to an unsigned long
+  }
+
 
   // Initialise variables for processing input text
   char inputChar {'x'};
@@ -88,6 +115,10 @@ int main(int argc, char* argv[])
     inpStream.close();
   }
 
+  //now run cipher on input text converting to a encrypted/decrypted output
+
+  std::string outputText{runCaesarCipher(inputText,cipherKey,encryptRequested)};
+
 
   
   
@@ -97,14 +128,15 @@ int main(int argc, char* argv[])
   // Warn that output file option not yet implemented
   if (outputFile.empty()) {
     std::cout << "No output file stated, using stdout"<<std::endl;
-    std::cout <<std::endl<< inputText << std::endl;
+    std::cout <<std::endl<< outputText << std::endl;
   }else{
     std::ofstream outStream{outputFile,std::ios::app};
     if (!outStream.good()){
       std::cerr<<"File "<<outputFile<<" could not be opened correctly"<<"...Exiting"<<std::endl;
       return 1;
     }
-    outStream<<std::endl<<inputText;
+    outStream<<std::endl<<outputText; //output written out to new line
+
     outStream.close();
   }
 
